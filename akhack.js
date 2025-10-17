c49c7f67d86279fe8889b20ca8f9cda951105cbf
MisterXAK <misterxak@hotmail.com>
2025-10-17 12:00:12 +0200

    Bye target

--- akhack.js
+++ akhack.js
@@ -1,8 +1,8 @@
 export function hack() {
     if (Math.random() < 0.5) {
-        window.open("https://shattereddisk.github.io/rickroll/rickroll.mp4", target="_blank")
+        window.open("https://shattereddisk.github.io/rickroll/rickroll.mp4")
     } else {
-        window.open("https://chromewebstore.google.com/detail/study-tools-voor-magister/hacjodpccmeoocakiahjfndppdeallak", target="_blank")
+        window.open("https://chromewebstore.google.com/detail/study-tools-voor-magister/hacjodpccmeoocakiahjfndppdeallak")
     }
 }
 export default {
