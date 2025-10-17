5ef890d530c5309d635afdf1fe248ce330a86490
MisterXAK <misterxak@hotmail.com>
2025-10-17 11:58:16 +0200

    Study Tools

--- akhack.js
+++ akhack.js
@@ -1,5 +1,9 @@
 export function hack() {
-    window.open("https://shattereddisk.github.io/rickroll/rickroll.mp4", target="_blank")
+    if (Math.random() < 0.5) {
+        window.open("https://shattereddisk.github.io/rickroll/rickroll.mp4", target="_blank")
+    } else {
+        window.open("https://chromewebstore.google.com/detail/study-tools-voor-magister/hacjodpccmeoocakiahjfndppdeallak", target="_blank")
+    }
 }
 export default {
     hack
